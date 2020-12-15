4d43a92e2f4d36a063cbc97f6285cba020d98757
polRk <polrkov.tm@gmail.com>
2020-12-15 19:21:41 +0300

    feat: update home screen

--- homescreen.pas
+++ homescreen.pas
@@ -58,19 +58,23 @@ type
     CouplesPage: TTabSheet;
     BridesListPopup: TPopupMenu;
 
-    procedure BridesActionsToggle(Sender: TObject);
-    procedure FileSaveExecute(Sender: TObject);
-    procedure GroomsActionsToggle(Sender: TObject);
 
     procedure CouplesPageHide(Sender: TObject);
     procedure CouplesPageShow(Sender: TObject);
-    procedure FileOpenAccept(Sender: TObject);
-    procedure FileSaveAsAccept(Sender: TObject);
+
+    procedure onBridePopup(Sender: TObject);
+
+    procedure OnGroomPopup(Sender: TObject);
+
+    procedure OnFileSave(Sender: TObject);
+
+    procedure OnFileOpen(Sender: TObject);
+    procedure OnFileSaveAs(Sender: TObject);
     procedure FormClose(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure PartnerAddExecute(Sender: TObject);
-    procedure PartnerEditExecute(Sender: TObject);
-    procedure PartnerRemoveExecute(Sender: TObject);
+    procedure OnPartnerAdd(Sender: TObject);
+    procedure OnPartnerEdit(Sender: TObject);
+    procedure OnPartnerRemove(Sender: TObject);
   private
 
   public
@@ -90,130 +94,14 @@ uses
 
 { THomeForm }
 
-procedure THomeForm.BridesActionsToggle(Sender: TObject);
-begin
-  FileSaveAs.Enabled := False;
-  FileSave.Enabled := False;
-  PartnerEdit.Enabled := False;
-  PartnerRemove.Enabled := False;
-
-  if listOfBrides <> nil then
-    FileSaveAs.Enabled := True;
-
-  if bridesFN <> '' then
-    FileSave.Enabled := True;
-
-  if BridesListBox.GetSelectedText <> '' then
-  begin
-    PartnerEdit.Enabled := True;
-    PartnerRemove.Enabled := True;
-  end;
-end;
-
-procedure THomeForm.GroomsActionsToggle(Sender: TObject);
-begin
-  FileSaveAs.Enabled := False;
-  FileSave.Enabled := False;
-  PartnerEdit.Enabled := False;
-  PartnerRemove.Enabled := False;
-
-  if listOfGrooms <> nil then
-    FileSaveAs.Enabled := True;
-
-  if groomsFN <> '' then
-    FileSave.Enabled := True;
-
-  if GroomsListBox.GetSelectedText <> '' then
-  begin
-    PartnerEdit.Enabled := True;
-    PartnerRemove.Enabled := True;
-  end;
-end;
-
-procedure THomeForm.CouplesPageHide(Sender: TObject);
-begin
-  MenuEditGroup.Enabled := True;
-  FileOpen.Enabled := True;
-  FileSave.Enabled := False;
-
-  CoupleStack.Free(listOfCouples);
-end;
-
-procedure THomeForm.CouplesPageShow(Sender: TObject);
-begin
-  MenuEditGroup.Enabled := False;
-  FileOpen.Enabled := False;
-  FileSave.Enabled := False;
-
-  if couplesFN <> '' then
-    FileSave.Enabled := True;
-
-  listOfCouples := FindMaxCouples(listOfBrides, listOfGrooms);
-  DrawListOfCouples(listOfCouples, CouplesListBox);
-end;
-
-procedure THomeForm.FileOpenAccept(Sender: TObject);
-begin
-  if Pages.ActivePageIndex = BridesPage.PageIndex then
-  begin
-    bridesFN := FileOpen.Dialog.FileName;
-
-    ProcessPartnersFileOpen(bridesFN, listOfBrides, BridesListBox);
-  end;
-
-  if Pages.ActivePageIndex = GroomsPage.PageIndex then
-  begin
-    groomsFN := FileOpen.Dialog.FileName;
-
-    ProcessPartnersFileOpen(groomsFN, listOfGrooms, GroomsListBox);
-  end;
-end;
-
-procedure THomeForm.FileSaveExecute(Sender: TObject);
-begin
-  if (Pages.ActivePageIndex = BridesPage.PageIndex) and (bridesFN <> '') then
-  begin
-    ProcessPartnersFileSave(bridesFN, listOfBrides);
-  end;
-
-  if (Pages.ActivePageIndex = GroomsPage.PageIndex) and (groomsFN <> '') then
-  begin
-    ProcessPartnersFileSave(groomsFN, listOfGrooms);
-  end;
-
-  if (Pages.ActivePageIndex = CouplesPage.PageIndex) and (couplesFN <> '') then
-  begin
-    ProcessCouplesFileSave(couplesFN, listOfCouples);
-  end;
-end;
-
-procedure THomeForm.FileSaveAsAccept(Sender: TObject);
-begin
-  FileSave.Enabled := True;
-
-  if Pages.ActivePageIndex = BridesPage.PageIndex then
-  begin
-    bridesFN := FileSaveAs.Dialog.FileName;
-    ProcessPartnersFileSave(bridesFN, listOfBrides);
-  end;
-
-  if Pages.ActivePageIndex = GroomsPage.PageIndex then
-  begin
-    groomsFN := FileSaveAs.Dialog.FileName;
-    ProcessPartnersFileSave(groomsFN, listOfGrooms);
-  end;
-
-  if Pages.ActivePageIndex = CouplesPage.PageIndex then
-  begin
-    couplesFN := FileSaveAs.Dialog.FileName;
-    ProcessCouplesFileSave(couplesFN, listOfCouples);
-  end;
-end;
+{ События формы }
 
 procedure THomeForm.FormClose(Sender: TObject);
 begin
+  // Удаляю стэки из памяти
   PartnerStack.Free(listOfBrides);
   PartnerStack.Free(listOfGrooms);
+  CoupleStack.Free(listOfCouples);
 end;
 
 procedure THomeForm.FormCreate(Sender: TObject);
@@ -221,14 +109,185 @@ begin
   Pages.PageIndex := 0;
 end;
 
-procedure THomeForm.PartnerAddExecute(Sender: TObject);
+procedure THomeForm.CouplesPageHide(Sender: TObject);
+begin
+  // Сбразываю доступность пунктов меню
+  FileOpen.Enabled := True;
+  FileSave.Enabled := False;
+  FileSaveAs.Enabled := False;
+  MenuEditGroup.Enabled := True;
+
+  // Очищаю из памяти стэк пар
+  CoupleStack.Free(listOfCouples);
+end;
+
+procedure THomeForm.CouplesPageShow(Sender: TObject);
+begin
+  // Сбразываю доступность пунктов меню
+  FileOpen.Enabled := False;
+  FileSave.Enabled := False;
+  FileSaveAs.Enabled := False;
+  MenuEditGroup.Enabled := False;
+
+  // Если пользователь открыл файл со списком пар,
+  // то позволяю сохранить стэк в уже открытый файл
+  if couplesFN <> '' then
+    FileSave.Enabled := True;
+
+  // Записываю в глобальную переменную стэк пар
+  listOfCouples := FindMaxCouples(listOfBrides, listOfGrooms);
+
+  // Если стэк не пуст, то позволяю сохранить его в файл
+  if listOfCouples <> nil then
+    FileSaveAs.Enabled := True;
+
+  // Отрисовываю стэк пар на экране
+  Variables.DrawListOfCouples(listOfCouples, CouplesListBox);
+end;
+
+{ Работа с файлами }
+
+{ Обработчик открытия списка из файла }
+procedure THomeForm.OnFileOpen(Sender: TObject);
+begin
+  // Показываю пользователю диалог выбора места хранения
+  // файла со списком партнеров.
+  // Сохраняю путь до файла в глобальную переменную
+
+  if Pages.ActivePageIndex = BridesPage.PageIndex then
+  begin
+    bridesFN := FileOpen.Dialog.FileName;
+
+    Variables.ProcessPartnersFileOpen(bridesFN, listOfBrides, BridesListBox);
+  end;
+
+  if Pages.ActivePageIndex = GroomsPage.PageIndex then
+  begin
+    groomsFN := FileOpen.Dialog.FileName;
+
+    Variables.ProcessPartnersFileOpen(groomsFN, listOfGrooms, GroomsListBox);
+  end;
+end;
+
+{ Обработчик сохранения списка в открытый файл }
+procedure THomeForm.OnFileSave(Sender: TObject);
+begin
+  // Сохраняю стэк в открытый файл
+
+  if (Pages.ActivePageIndex = BridesPage.PageIndex) and (bridesFN <> '') then
+  begin
+    Variables.ProcessPartnersFileSave(bridesFN, listOfBrides);
+  end;
+
+  if (Pages.ActivePageIndex = GroomsPage.PageIndex) and (groomsFN <> '') then
+  begin
+    Variables.ProcessPartnersFileSave(groomsFN, listOfGrooms);
+  end;
+
+  if (Pages.ActivePageIndex = CouplesPage.PageIndex) and (couplesFN <> '') then
+  begin
+    Variables.ProcessCouplesFileSave(couplesFN, listOfCouples);
+  end;
+end;
+
+{ Обработчик сохранения списка в новый файл }
+procedure THomeForm.OnFileSaveAs(Sender: TObject);
+begin
+  // Показываю пользователю диалог выбора места назначения
+  // для нового файла (или существующего, тогда произойдет перезапись),
+  // и сохраняю стэк в выбранный файл
+
+  if Pages.ActivePageIndex = BridesPage.PageIndex then
+  begin
+    bridesFN := FileSaveAs.Dialog.FileName;
+    Variables.ProcessPartnersFileSave(bridesFN, listOfBrides);
+  end;
+
+  if Pages.ActivePageIndex = GroomsPage.PageIndex then
+  begin
+    groomsFN := FileSaveAs.Dialog.FileName;
+    Variables.ProcessPartnersFileSave(groomsFN, listOfGrooms);
+  end;
+
+  if Pages.ActivePageIndex = CouplesPage.PageIndex then
+  begin
+    couplesFN := FileSaveAs.Dialog.FileName;
+    Variables.ProcessCouplesFileSave(couplesFN, listOfCouples);
+  end;
+
+  // Включаю процедуру сохранения в файл
+  FileSave.Enabled := True;
+end;
+
+{ Работа с партнерами }
+
+{ Обработчик отображение popup menu на странице невест }
+procedure THomeForm.onBridePopup(Sender: TObject);
+begin
+  // Сбразываю доступность пунктов меню
+  FileSave.Enabled := False;
+  FileSaveAs.Enabled := False;
+  PartnerEdit.Enabled := False;
+  PartnerRemove.Enabled := False;
+
+  // Если стэк невест не пуст,
+  // то позволяю сохранить стэк в новый файл
+  if listOfBrides <> nil then
+    FileSaveAs.Enabled := True;
+
+  // Если пользователь открыл файл со списком невест,
+  // то позволяю сохранить стэк в уже открытый файл
+  if bridesFN <> '' then
+    FileSave.Enabled := True;
+
+  // Если пользователь выбрал невесту в списке,
+  // то позволяю её редактирование и удаление
+  if BridesListBox.GetSelectedText <> '' then
+  begin
+    PartnerEdit.Enabled := True;
+    PartnerRemove.Enabled := True;
+  end;
+end;
+
+{ Обработчик отображение popup menu на странице женихов }
+procedure THomeForm.OnGroomPopup(Sender: TObject);
+begin
+  // Сбразываю доступность пунктов меню
+  FileSave.Enabled := False;
+  FileSaveAs.Enabled := False;
+  PartnerEdit.Enabled := False;
+  PartnerRemove.Enabled := False;
+
+  // Если стэк женихов не пуст,
+  // то позволяю сохранить стэк в новый файл
+  if listOfGrooms <> nil then
+    FileSaveAs.Enabled := True;
+
+  // Если пользователь открыл файл со списком женихов,
+  // то позволяю сохранить стэк в уже открытый файл
+  if groomsFN <> '' then
+    FileSave.Enabled := True;
+
+  // Если пользователь выбрал жениха в списке,
+  // то позволяю его редактирование и удаление
+  if GroomsListBox.GetSelectedText <> '' then
+  begin
+    PartnerEdit.Enabled := True;
+    PartnerRemove.Enabled := True;
+  end;
+end;
+
+{ Добавить партнера }
+procedure THomeForm.OnPartnerAdd(Sender: TObject);
 var
   Result: TModalResult;
 begin
+  // Создаю форму редактирования партнера
   EditForm := TEditForm.Create(Application);
   EditForm.Left := HomeForm.Left;
   EditForm.Top := HomeForm.Top;
 
+  // Изменяю заголовок формы в зависимости от активной страницы
   if Pages.ActivePageIndex = BridesPage.PageIndex then
   begin
     currentPartner := PartnerStack.NewPartner('', PartnerSex.woman);
@@ -241,83 +300,116 @@ begin
     EditForm.Caption := 'Добавить жениха';
   end;
 
+  // Показываю форму редактирования, и, если пользователь нажал
+  // на кнопку закрытия окна или не заполнил имя, то выхожу из функции
   Result := EditForm.ShowModal;
   if (Result = mrCancel) or (currentPartner.FullName = '') then
     Exit;
 
+  // Добавляю невесту в стэк и в список на экране
   if Pages.ActivePageIndex = BridesPage.PageIndex then
-    AddPartner(currentPartner, listOfBrides, BridesListBox);
+    Variables.AddPartner(currentPartner, listOfBrides, BridesListBox);
 
+  // Добавляю жениха в стэк и в список на экране
   if Pages.ActivePageIndex = GroomsPage.PageIndex then
-    AddPartner(currentPartner, listOfGrooms, GroomsListBox);
+    Variables.AddPartner(currentPartner, listOfGrooms, GroomsListBox);
 
 end;
 
-procedure THomeForm.PartnerEditExecute(Sender: TObject);
+{ Обработчик события редактирования выбранного партнера }
+procedure THomeForm.OnPartnerEdit(Sender: TObject);
 var
   Result: TModalResult;
   TempNode: PartnerNode = nil;
 begin
+  // Создаю форму редактирования партнера
   EditForm := TEditForm.Create(Application);
   EditForm.Left := HomeForm.Left;
   EditForm.Top := HomeForm.Top;
 
+  // Изменяю заголовок формы в зависимости от активной страницы
   if Pages.ActivePageIndex = BridesPage.PageIndex then
     EditForm.Caption := 'Редактирова невесту';
   if Pages.ActivePageIndex = GroomsPage.PageIndex then
     EditForm.Caption := 'Редактировать жениха';
 
+  // Сохраняю ссылку на узел с выбранным партнером
   if Pages.ActivePageIndex = BridesPage.PageIndex then
     TempNode := FindPartner(BridesListBox.GetSelectedText, listOfBrides);
 
   if Pages.ActivePageIndex = GroomsPage.PageIndex then
     TempNode := FindPartner(GroomsListBox.GetSelectedText, listOfGrooms);
 
+  // Если партнер не найден, то выхожу
   if TempNode = nil then
     Exit;
 
+  // Записываю партнера в глобальную переменную
   currentPartner := TempNode^.Data;
 
+  // Показываю форму редактирования, и, если пользователь нажал
+  // на кнопку закрытия окна, то выхожу из функции
   Result := EditForm.ShowModal;
   if Result = mrCancel then
     Exit;
 
+  // Обновляю партнера
   if Pages.ActivePageIndex = BridesPage.PageIndex then
   begin
+    // Удаляю невесту из стэка
     PartnerStack.Delete(listOfBrides, TempNode);
-    AddPartner(currentPartner, listOfBrides, BridesListBox);
+    // Добавляю в стэк и в список на экране измененную невесту
+    Variables.AddPartner(currentPartner, listOfBrides, BridesListBox);
+    // Сбрасываю индекс выбранной строки в списке на экране
     BridesListBox.ItemIndex := -1;
   end;
 
   if Pages.ActivePageIndex = GroomsPage.PageIndex then
   begin
+    // Удаляю жениха из стэка
     PartnerStack.Delete(listOfGrooms, TempNode);
-    AddPartner(currentPartner, listOfGrooms, GroomsListBox);
+    // Добавляю в стэк и в список на экране измененного жениха
+    Variables.AddPartner(currentPartner, listOfGrooms, GroomsListBox);
+    // Сбрасываю индекс выбранной строки в списке на экране
     GroomsListBox.ItemIndex := -1;
   end;
 end;
 
-procedure THomeForm.PartnerRemoveExecute(Sender: TObject);
+{ Обработчик события удаления выбранного партнера }
+procedure THomeForm.OnPartnerRemove(Sender: TObject);
 var
   P: PartnerNode;
 begin
+  // Запрашиваю подтверждение удаления
   if MessageDlg('Подтвердите удаление', mtConfirmation,
     [mbYes, mbNo], 0) <> mrYes then
     Exit;
 
+  // Если активная страница - страница со списком невест,
+  // то удаляю выбранную невесту из списка на экарне и стэка
   if Pages.ActivePageIndex = BridesPage.PageIndex then
   begin
+    // Ищу указатель на звено в стэке невест
     P := FindPartner(BridesListBox.GetSelectedText, listOfBrides);
+    // Удаляю звено из стэка по указателю
     PartnerStack.Delete(listOfBrides, P);
+    // Удаляю из списка на экране выбранную невесту
     BridesListBox.Items.Delete(BridesListBox.ItemIndex);
+    // Сбрасываю индекс выбранной строки в списке на экране
     BridesListBox.ItemIndex := -1;
   end;
 
+  // Если активная страница - страница со списком женихов,
+  // то удаляю выбранного жениха из списка на экране и стэка
   if Pages.ActivePageIndex = GroomsPage.PageIndex then
   begin
+    // Ищу указатель на звено в стэке женихов
     P := FindPartner(GroomsListBox.GetSelectedText, listOfGrooms);
+    // Удаляю звено из стэка по указателю
     PartnerStack.Delete(listOfGrooms, P);
+    // Удаляю из списка на экране выбранного жениха
     GroomsListBox.Items.Delete(GroomsListBox.ItemIndex);
+    // Сбрасываю индекс выбранной строки в списке на экране
     GroomsListBox.ItemIndex := -1;
   end;
 end;
